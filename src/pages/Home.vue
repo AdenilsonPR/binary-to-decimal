d2cad8472942c9e91b0eebcec9fb04c4555e474e
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2020-02-23 16:32:52 -0300

    mod: Form color end shadow

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <q-page>
-
+    <div class="fixed-center z-top">
+      <p class="text-white text-center text-h4">Binary to decimal converter</p>
+      <div class="form-main shadow-1 float-center"></div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home"
+};
 </script>
+
+<style>
+.form-main {
+  height: 50vmin;
+  width: 75vmin;
+  background-color: white;
+  margin: auto;
+  border-radius: 0.5em;
+}
+</style>
