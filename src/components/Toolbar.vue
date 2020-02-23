5b494aa22be944458b565da9e3701f9f8d56718c
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2020-02-23 18:06:06 -0300

    mod: Remove toolbar name

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -5,21 +5,11 @@
       <q-btn flat icon="eva-github-outline" label="Github" />
       <q-btn flat icon="eva-linkedin-outline" label="LinkedIn" />
     </q-toolbar>
-    <q-toolbar class="z-top">
-      <q-toolbar-title>
-        <p class="text-h4 q-ml-md">{{ myName }}</p>
-      </q-toolbar-title>
-    </q-toolbar>
   </div>
 </template>
 
 <script>
 export default {
   name: "Toolbar",
-  data () {
-    return{
-      myName: 'Adenilson Prado Raul'
-    }
-  }
 };
 </script>
