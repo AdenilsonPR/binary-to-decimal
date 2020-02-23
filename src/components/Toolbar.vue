611eec9c2b675a88cb477e31653d5950f195550d
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2020-02-23 13:20:20 -0300

    add: Toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,14 +1,25 @@
 <template>
-
+  <div class="bg-orange text-white">
+    <q-toolbar>
+      <q-space />
+      <q-btn flat icon="eva-github-outline" label="Github" />
+      <q-btn flat icon="eva-linkedin-outline" label="LinkedIn" />
+    </q-toolbar>
+    <q-toolbar inset>
+      <q-toolbar-title>
+        <p class="text-h4">{{ myName }}</p>
+      </q-toolbar-title>
+    </q-toolbar>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Toolbar",
-  data() {
-    return {
-
-    };
+  data () {
+    return{
+      myName: 'Adenilson Prado Raul'
+    }
   }
 };
 </script>
