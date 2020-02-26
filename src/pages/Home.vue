805f8158044461addd42c5f36fac2a4e72989805
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2020-02-25 21:01:56 -0300

    add: Validation if the numbers are different from 0 or 1.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,7 +7,14 @@
           <div class="col-12">
             <p class="text-center">Enter a number with binary base to be converted</p>
           </div>
-          <q-input outlined v-model="bin" class="col-12">
+          <q-input
+            outlined
+            v-model="bin"
+            class="col-12"
+            type="number"
+            :error-message="msg"
+            :error="!isValid"
+          >
             <template v-slot:append>
               <q-icon name="close" @click="bin = ''" />
             </template>
@@ -19,6 +26,7 @@
             label="Calcular"
             color="deep-orange-8"
             @click="calculate()"
+            :disable="!isValid"
           />
         </div>
       </div>
@@ -35,7 +43,8 @@ export default {
   data() {
     return {
       bin: "",
-      dec: 0
+      dec: 0,
+      msg: "Just enter numbers 1 or 0."
     };
   },
 
@@ -45,6 +54,15 @@ export default {
         this.dec = binToDec(this.bin);
       } else {
         this.dec = 0;
+      }
+    }
+  },
+  computed: {
+    isValid() {
+      if (this.bin.search(/[2-9]/) > -1) {
+        return false;
+      } else {
+        return true;
       }
     }
   }
